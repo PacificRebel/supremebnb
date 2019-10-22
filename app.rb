3cfd5bd824b857acb774f610742504a7a7b8bf29
Heli Sivunen <heli@mooncat.tv>
2019-10-22 11:07:26 +0100

    Add form for listing spaces.

--- app.rb
+++ app.rb
@@ -5,13 +5,21 @@ require_relative 'lib/space'
 class SupremeBNB < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/users' do
-  User.create(:username => params[:username] )
-  redirect '/'
-end
+  post '/users' do
+    User.create(username: params[:username] )
+    redirect '/'
+  end
+
+  get '/spaces/new' do
+    erb :'spaces/new'
+  end
+
+  post '/spaces/new' do
+    Space.create(name: params[:name], description: params[:description], price: params[:price])
+  end
 
 end
