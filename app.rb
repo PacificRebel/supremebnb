d706a6bf4c6683fa7c15632a42ce7fff39ab5f9d
Heli Sivunen <heli@mooncat.tv>
2019-10-22 10:27:44 +0100

    Add index page with form to sign up, which adds username to database.

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ require_relative 'lib/space'
 class SupremeBNB < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+get '/' do
+  erb :index
+end
+
+post '/users' do
+  User.create(:username => params[:username] )
+  redirect '/'
+end
+
 end
