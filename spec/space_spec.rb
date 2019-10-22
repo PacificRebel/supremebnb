7771946d03f1620f6832943995177e2890137e5d
kevindhanna <kevindhanna@gmail.com>
2019-10-22 11:46:34 +0100

    remove commented lines from space spec

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -3,7 +3,6 @@ describe Space do
 
   describe '.create' do
     it 'creates a new space with price, name and description properties' do
-      # space = Space.create(price: 20, name: "Central London Apartment", description: "Cosy and small", start_date: "2019-06-05", end_date: "2020-06-05")
       expect(space.price).to eq 20
       expect(space.name).to eq "Central London Apartment"
       expect(space.description).to eq "Cosy and small"
